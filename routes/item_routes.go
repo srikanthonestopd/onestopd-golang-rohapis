d476fa38684b4c1c631f7edb5f02e0d01e30cb64
srikanthonestopd <srikanth@onestopd.com>
2025-02-20 23:16:15 -0700

    new updates

--- routes/item_routes.go
+++ routes/item_routes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"onestopd-golang-rohapis/handlers"
+)
+
+func RegisterItemRoutes(router *mux.Router) {
+	router.HandleFunc("/api/insert", handlers.InsertData).Methods("POST")
+	router.HandleFunc("/api/get/{id}", handlers.GetData).Methods("GET")
+	router.HandleFunc("/api/next-id", handlers.GetNextItemIDHandler).Methods("GET")
+
+}
--- routes/order_routes.go
+++ routes/order_routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"onestopd-golang-rohapis/handlers"
+)
+
+func RegisterOrderRoutes(router *mux.Router) {
+	router.HandleFunc("/api/orders", handlers.GetOrders).Methods("GET")
+	router.HandleFunc("/api/orders", handlers.AddOrder).Methods("POST")
+}
